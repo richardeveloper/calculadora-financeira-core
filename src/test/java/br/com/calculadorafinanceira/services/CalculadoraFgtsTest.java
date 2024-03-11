e930ca88351489ef3890722e4340d05a5be10bc9
richardeveloper <richard_ar@hotmail.com>
2024-03-11 13:24:07 -0300

    Correção testes unitários

--- src/test/java/br/com/calculadorafinanceira/services/CalculadoraFgtsTest.java
+++ src/test/java/br/com/calculadorafinanceira/services/CalculadoraFgtsTest.java
@@ -2,21 +2,28 @@ package br.com.calculadorafinanceira.services;
 
 import br.com.calculadorafinanceira.exceptions.models.ServiceException;
 import br.com.calculadorafinanceira.requests.FgtsRequest;
+import br.com.calculadorafinanceira.requests.JurosCompostosRequest;
 import br.com.calculadorafinanceira.responses.FgtsResponse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.when;
 
 class CalculadoraFgtsTest {
+
+  @Spy
+  CalculadoraJuros calculadoraJuros;
 
   @InjectMocks
   CalculadoraFgts calculadoraFgts;
@@ -24,6 +31,24 @@ class CalculadoraFgtsTest {
   @BeforeEach
   void setUp() {
     MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void calcularFgts_deveCalcularValorFgtsComSucesso() {
+
+    FgtsRequest request = new FgtsRequest();
+    request.setSalarioBruto(new BigDecimal("50.00"));
+    request.setDataEntrada(LocalDate.now().minusMonths(5));
+    request.setDataSaida(LocalDate.now());
+
+    FgtsResponse response = calculadoraFgts.calcularFgts(request);
+
+    assertThat(response).isNotNull();
+
+    assertThat(response.getDepositoMensal()).isEqualTo(new BigDecimal("4.00"));
+    assertThat(response.getJurosCorrecao()).isEqualTo(new BigDecimal("0.14"));
+    assertThat(response.getTotalDepositado()).isEqualTo(new BigDecimal("20.00"));
+    assertThat(response.getTotalCorrigido()).isEqualTo(new BigDecimal("20.14"));
   }
 
   @Test
@@ -39,21 +64,6 @@ class CalculadoraFgtsTest {
     String expectedMessage = "A data de saída do colaborador na empresa deve ser superior a data de entrada.";
 
     assertThat(expectedMessage).isEqualTo(exception.getMessage());
-  }
-
-  @Test
-  void calcularFgts_deveCalcularValorFgtsComSucesso() {
-
-    FgtsRequest request = new FgtsRequest();
-    request.setSalarioBruto(new BigDecimal("50.00"));
-    request.setDataEntrada(LocalDate.now().minusMonths(5));
-    request.setDataSaida(LocalDate.now());
-
-    FgtsResponse response = calculadoraFgts.calcularFgts(request);
-
-    assertThat(response).isNotNull();
-    assertThat(response.getDepositoMensal()).isEqualTo(new BigDecimal("4.00"));
-    assertThat(response.getTotalDepositado()).isEqualTo(new BigDecimal("20.00"));
   }
 
 }
